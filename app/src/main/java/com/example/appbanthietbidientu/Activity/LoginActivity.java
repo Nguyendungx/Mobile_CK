1c9d31e4e61f90177af57b4e8a34eeb7c59e6c64
qdatn <datnq2003@gmail.com>
2024-04-29 21:52:28 +0700

    Phân quyền user admin

--- app/src/main/java/com/example/appbanthietbidientu/Activity/LoginActivity.java
+++ app/src/main/java/com/example/appbanthietbidientu/Activity/LoginActivity.java
@@ -14,17 +14,25 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.appbanthietbidientu.R;
+import com.example.appbanthietbidientu.model.Account;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText emailedit, passedit;
     private Button btn_login, btn_signup;
     private FirebaseAuth mAuth;
+//    private FirebaseDatabase db;
+    private DatabaseReference accountRef;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -77,11 +85,41 @@ public class LoginActivity extends AppCompatActivity {
                 if (user != null) {
                     String uid = user.getUid();
                     Log.d("User UID", "UID: " + uid);
+
+                    accountRef = FirebaseDatabase.getInstance().getReference().child("account");
+
+                    accountRef.child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            // lấy vai trò từ account vừa login
+                            int role = snapshot.child("role").getValue(Integer.class);
+
+                            // Kiểm tra vai trò và chuyển hướng tới layout tương ứng
+                            // role 1 = admin, role 2 = user
+                            if (role == 1) {
+                                // layout admin
+                                Intent intent = new Intent(LoginActivity.this, AdminActivity.class);
+                                startActivity(intent);
+                            } else if (role == 2) {
+                                // layout user
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            } else {
+                                // Trường hợp vai trò không xác định
+                                Log.e("Role Error", "Undefined role for user: " + uid);
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
                 }
 
 
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                startActivity(intent);
             } else {
                 Toast.makeText(getApplicationContext(), "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
             }
